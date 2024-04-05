b3b4f396c7d649be1ec37218428b99cc90feb7d5
Smi-lin <olajideolabimpe26@gmail.com>
2024-04-05 12:45:48 +0100

    media query

--- frontend/components/HomePage/CurrentAndUpcoming/CurrentAndUpcoming.js
+++ frontend/components/HomePage/CurrentAndUpcoming/CurrentAndUpcoming.js
@@ -6,112 +6,113 @@ const CurrentAndUpcoming = () => {
         Current and Upcoming Programmmes.
       </h1>
 
-      <div className="flex justify-center gap-[50px] items-center  flex-col xl:flex-row lg:flex-row">
-        <div className="max-w-xs shadow-lg flex flex-col items-center  p-5 rounded-lg border border-orange-300 w-[243px]">
+      <div className="flex justify-center flex-wrap gap-[44px] lg:mb-[135px] md:mb-[40px] sm:mb-[67px]">
+        <div className="max-w-xs shadow-lg flex flex-col items-center gap-4 p-5 rounded-lg border border-orange-300 w-[243px]">
           <img
-            className="h-[100px] w-[100px] rounded-full "
+            className="h-[150px] w-[150px] rounded-full "
             src="https://tinyurl.com/ytcbk3ej"
             alt=""
           />
-          <div className="px-6 py-4 mt-[5px] flex flex-col justify-center items-center gap-[35px]">
-            <div className=" text-center">
-              <h3 className="text-center font-poppins text-[22px] font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
+          <div className=" mt-[25px] pb-[31px] flex flex-col justify-center items-center">
+            <div className="w-[266px]  flex flex-col gap-2 justify-center items-center">
+              <h3 className="text-center font-poppins text-xl font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
                 Rorem Ipsum dolor
               </h3>
-              <p className="font-poppins text-[#1c1c1c] text-[16px] font-light opacity-65 leading-relaxed p-[15px]">
+
+              <p className="font-poppins text-[16px] text-sm font-light opacity-65 leading-relaxed w-[110px">
                 Web3 | Online
               </p>
+
               <p className="font-poppins text-neutral-black text-[16px] font-light opacity-65 leading-relaxed">
                 9/1/24 | 4 weeks
               </p>
-
-
             </div>
-            <button className="flex items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
+
+            <button className="flex mt-[35px] items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
               View more
             </button>
-
           </div>
         </div>
-        <div className="max-w-xs shadow-lg flex flex-col items-center  p-5 rounded-lg border border-orange-300 w-[243px]">
+
+        <div className="max-w-xs shadow-lg flex flex-col items-center gap-4 p-5 rounded-lg border border-orange-300 w-[243px]">
           <img
-            className="h-[100px] w-[100px] rounded-full "
-            src="https://tinyurl.com/yc5mmukd"
+            className="h-[150px] w-[150px] rounded-full "
+            src="https://tinyurl.com/ytcbk3ej"
             alt=""
           />
-          <div className="px-6 py-4 mt-[5px] flex flex-col justify-center items-center gap-[35px]">
-            <div className=" text-center">
-              <h3 className="text-center font-poppins text-[22px] font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
-              Korem ipsum dolor 
+          <div className=" mt-[25px] pb-[31px] flex flex-col justify-center items-center">
+            <div className="w-[266px]  flex flex-col gap-2 justify-center items-center">
+              <h3 className="text-center font-poppins text-xl font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
+                Rorem Ipsum dolor
               </h3>
-              <p className="font-poppins text-[#1c1c1c] text-[16px] font-light opacity-65 leading-relaxed p-[15px]">
+
+              <p className="font-poppins text-[16px] text-sm font-light opacity-65 leading-relaxed w-[110px">
                 Web3 | Online
               </p>
+
               <p className="font-poppins text-neutral-black text-[16px] font-light opacity-65 leading-relaxed">
                 9/1/24 | 4 weeks
               </p>
-
-
             </div>
-            <button className="flex items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
+
+            <button className="flex mt-[35px] items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
               View more
             </button>
-
           </div>
         </div>
-        <div className="max-w-xs shadow-lg flex flex-col items-center p-5 rounded-lg border border-orange-300 w-[243px]">
+
+        <div className="max-w-xs shadow-lg flex flex-col items-center gap-4 p-5 rounded-lg border border-orange-300 w-[243px]">
           <img
-            className="h-[100px] w-[100px] rounded-full "
-            src="https://tinyurl.com/yeykuw2x"
+            className="h-[150px] w-[150px] rounded-full "
+            src="https://tinyurl.com/ytcbk3ej"
             alt=""
           />
-          <div className="px-6 py-4 mt-[5px] flex flex-col justify-center items-center gap-[35px]">
-            <div className=" text-center">
-              <h3 className="text-center font-poppins text-[22px] font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
-              Yorem ipsum dolor 
+          <div className=" mt-[25px] pb-[31px] flex flex-col justify-center items-center">
+            <div className="w-[266px]  flex flex-col gap-2 justify-center items-center">
+              <h3 className="text-center font-poppins text-xl font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
+                Rorem Ipsum dolor
               </h3>
-              <p className="font-poppins text-[#1c1c1c] text-[16px] font-light opacity-65 leading-relaxed p-[15px]">
+
+              <p className="font-poppins text-[16px] text-sm font-light opacity-65 leading-relaxed w-[110px">
                 Web3 | Online
               </p>
+
               <p className="font-poppins text-neutral-black text-[16px] font-light opacity-65 leading-relaxed">
                 9/1/24 | 4 weeks
               </p>
-
-
             </div>
-            <button className="flex items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
+
+            <button className="flex mt-[35px] items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
               View more
             </button>
-
           </div>
         </div>
-        <div className="max-w-xs shadow-lg flex flex-col items-center p-5 rounded-lg border border-orange-300 w-[243px]">
+        <div className="max-w-xs shadow-lg flex flex-col items-center gap-4 p-5 rounded-lg border border-orange-300 w-[243px]">
           <img
-            className="h-[100px] w-[100px] rounded-full "
-            src="https://tinyurl.com/3wsufe7n"
+            className="h-[150px] w-[150px] rounded-full "
+            src="https://tinyurl.com/ytcbk3ej"
             alt=""
           />
-          <div className="px-6 py-4 mt-[5px] flex flex-col justify-center items-center gap-[35px]">
-            <div className=" text-center">
-              <h3 className="text-center font-poppins text-[22px] font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
-              Horem ipsum dolor 
+          <div className=" mt-[25px] pb-[31px] flex flex-col justify-center items-center">
+            <div className="w-[266px]  flex flex-col gap-2 justify-center items-center">
+              <h3 className="text-center font-poppins text-xl font-semibold text-neutral-black opacity-75 w-266 w-[233px] h-[26px]">
+                Rorem Ipsum dolor
               </h3>
-              <p className="font-poppins text-[#1c1c1c] text-[16px] font-light opacity-65 leading-relaxed p-[15px]">
+
+              <p className="font-poppins text-[16px] text-sm font-light opacity-65 leading-relaxed w-[110px">
                 Web3 | Online
               </p>
+
               <p className="font-poppins text-neutral-black text-[16px] font-light opacity-65 leading-relaxed">
                 9/1/24 | 4 weeks
               </p>
-
-
             </div>
-            <button className="flex items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
+
+            <button className="flex mt-[35px] items-center text-orange-500 bg-secondary-700 px-4 py-2 rounded-lg hover:bg-orange-100 focus:outline-none border border-orange-500 font-poppins text-base font-medium leading-relaxed">
               View more
             </button>
-
           </div>
         </div>
-
       </div>
     </div>
   );
