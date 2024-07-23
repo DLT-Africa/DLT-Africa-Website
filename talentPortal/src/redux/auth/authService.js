f51013a43365455286c05c0c2a833582b46dffa4
ameer017 <rajiabdullahi907@gmail.com>
2024-07-23 20:48:23 +0100

    talent-pool

---
+++ talentPortal/src/redux/auth/authService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
+export const API_URL = `${BACKEND_URL}/api/users/`;
+
+export const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
+const register = async (userData) => {
+  const response = await axios.post(API_URL + "register", userData);
+  return response.data;
+};
+
+const sendAutomatedEmail = async (emailData) => {
+  const response = await axios.post(API_URL + "sendAutomatedEmail", emailData);
+  return response.data.message;
+};
+
+const authService = {
+  register,
+  sendAutomatedEmail,
+};
+
+export default authService;
