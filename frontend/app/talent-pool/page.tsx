d19d32ccd5d21f7ca250796f1e66d3b15f91e6c1
ameer017 <rajiabdullahi907@gmail.com>
2024-07-24 12:51:37 +0100

    cors

---
+++ frontend/app/talent-pool/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+type SkillResponse = {
+  [key: string]: any;
+};
+
+const Talent: React.FC = () => {
+  const [availableSkills, setAvailableSkills] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchSkills = async () => {
+      try {
+        const response = await axios.get<SkillResponse>(
+          `https://dlt-backend.vercel.app/api/v1/skill/skills`
+        );
+        const skillCategories = Object.keys(response.data).filter(
+          (key) => key !== "_id" && key !== "__v"
+        );
+        setAvailableSkills(skillCategories);
+        console.log(availableSkills)
+      } catch (error) {
+        console.error("Error fetching skills:", error);
+        toast.error("Error fetching skills");
+      }
+    };
+
+    fetchSkills();
+  }, []);
+
+  return (
+    <section className="h-screen bg-[#f3f6f6] flex flex-col items-center">
+      <h1 className="text-[#441606] text-[36px] mt-[85px] mb-[61px] font-[400]">
+        DLT Africa Talent Pool
+      </h1>
+
+      <div className="">
+        <div>{/* buttons */}</div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default Talent;
