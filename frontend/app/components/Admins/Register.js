4c32f8913bd581283f5e76a3a121359579967b37
DLT-Africa <142330919+DLT-Africa@users.noreply.github.com>
2024-08-09 14:39:15 +0100

    all

--- frontend/app/components/Admins/Register.js
+++ frontend/app/components/Admins/Register.js
@@ -23,14 +23,14 @@ const Register = () => {
       setFormCompleted(true);
     }
   }, []);
-  const [formValidMessage, setFormValidMessage] = useState();
+
+  const [formValidMessage, setFormValidMessage] = useState("");
   const [formCompleted, setFormCompleted] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleChange = (e) => {
     setFormValidMessage("");
     const { name, value } = e.target;
-    console.log(e.target.value);
     setFormData({
       ...formData,
       [name]: value,
@@ -43,9 +43,12 @@ const Register = () => {
     const { name, email, password, phone } = formData;
 
     if (!name || !email || !password || !phone) {
-      setFormValidMessage("Oops! all fields are required");
+      setFormValidMessage("Oops! All fields are required.");
       return;
     }
+
+    // Add additional client-side validation here if necessary
+
     setIsSubmitting(true);
 
     axios
@@ -53,128 +56,126 @@ const Register = () => {
         `https://dlt-backend.vercel.app/api/v1/team/register-team`,
         formData
       )
-      .then(function (response) {
+      .then((response) => {
         console.log(response.data);
-        console.log(formData);
         setIsSubmitting(false);
         localStorage.setItem("isLoggedIn", "true");
         setFormCompleted(true);
         router.push("/admin-dashboard");
       })
-      .catch(function (error) {
+      .catch((error) => {
         setIsSubmitting(false);
-        if (error.response && error.response.status == 400) {
+        if (error.response && error.response.status === 400) {
           setFormValidMessage(
-            "Applicant with the same email address already exist"
+            "An applicant with the same email address already exists."
           );
         } else {
           setFormValidMessage(
-            "Server error unable to process your registration"
+            "Server error. Unable to process your registration."
           );
         }
       });
   };
+
   return (
-    <div>
-      <div className="mt-5 mb-20 p-4">
-        <Typography className="font-normal md:text-[36px] sm:text-[25px] text-black mb-[39px] text-center ">
-          Register as an Admin
-        </Typography>
-        <form
-          onSubmit={handleSubmit}
-          className=" rounded-2xl bg-[#FFEFD4] py-[69px] px-8 lg:px-[86px] mx-auto lg:min-w-[65%] 2xl:min-w-[50%] lg:max-w-[65%] 2xl:max-w-[50%]  "
+    <div className="mt-5 mb-20 p-4">
+      <Typography className="font-normal md:text-[36px] sm:text-[25px] text-black mb-[39px] text-center">
+        Register as an Admin
+      </Typography>
+      <form
+        onSubmit={handleSubmit}
+        className="rounded-2xl bg-[#FFEFD4] py-[69px] px-8 lg:px-[86px] mx-auto lg:min-w-[65%] 2xl:min-w-[50%] lg:max-w-[65%] 2xl:max-w-[50%]"
+      >
+        <div className="grid grid-cols-none md:grid-cols-none gap-y-14 gap-x-14 text-center">
+          <Input
+            size="lg"
+            name="name"
+            variant="static"
+            label="Name"
+            className="pl-4 text-xl"
+            labelProps={{
+              className: "!text-black",
+            }}
+            containerProps={{
+              className: "h-14",
+            }}
+            placeholder="Full Name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <Input
+            size="lg"
+            type="email"
+            name="email"
+            variant="static"
+            label="Email Address"
+            className="pl-4 text-xl"
+            labelProps={{
+              className: "!text-black",
+            }}
+            containerProps={{
+              className: "h-14",
+            }}
+            placeholder="yourname@gmail.com"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <Input
+            type="password"
+            size="lg"
+            name="password"
+            variant="static"
+            label="Password"
+            className="pl-4 text-xl"
+            labelProps={{
+              className: "!text-black",
+            }}
+            containerProps={{
+              className: "h-14",
+            }}
+            placeholder=""
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <Input
+            size="lg"
+            type="text"
+            name="phone"
+            variant="static"
+            label="Phone Number"
+            className="pl-4 text-xl"
+            labelProps={{
+              className: "!text-black",
+            }}
+            containerProps={{
+              className: "h-14",
+            }}
+            placeholder="08123456789"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </div>
+
+        <Button
+          type="submit"
+          size="large"
+          className="capitalize px-16 py-4 bg-[#FC7C13] my-[35px] w-full text-[16px] transition duration-500 ease-in-out transform hover:-translate-y-1"
         >
-          <div className="grid grid-cols-none md:grid-cols-none gap-y-14 gap-x-14 text-center">
-            <Input
-              size="lg"
-              name="firstName"
-              variant="static"
-              label="First Name"
-              className="pl-4 text-xl"
-              labelProps={{
-                className: "!text-black",
-              }}
-              containerProps={{
-                className: "h-14 ",
-              }}
-              placeholder="First Name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-            <Input
-              size="lg"
-              type="email"
-              name="email"
-              variant="static"
-              label="Email Address"
-              className="pl-4 text-xl"
-              labelProps={{
-                className: "!text-black",
-              }}
-              containerProps={{
-                className: "h-14 ",
-              }}
-              placeholder="yourname@gmail.com"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <Input
-              type="password"
-              size="lg"
-              name="password"
-              variant="static"
-              label="Password"
-              className="pl-4 text-xl"
-              labelProps={{
-                className: "!text-black",
-              }}
-              containerProps={{
-                className: "h-14 ",
-              }}
-              placeholder=""
-              value={formData.password}
-              onChange={handleChange}
-            />
-            <Input
-              size="lg"
-              type="text"
-              name="phone"
-              variant="static"
-              label="Phone Number"
-              className="pl-4 text-xl"
-              labelProps={{
-                className: "!text-black",
-              }}
-              containerProps={{
-                className: "h-14 ",
-              }}
-              placeholder="08123456789"
-              value={formData.phone}
-              onChange={handleChange}
-            />
+          {isSubmitting ? <Loader /> : <span>Register</span>}
+        </Button>
+        {formValidMessage && (
+          <div className="event-page-registration-error-message text-red-600 mt-4">
+            {formValidMessage}
           </div>
+        )}
 
-          <Button
-            type="submit"
-            size="large"
-            className="capitalize px-16 py-4 bg-[#FC7C13] my-[35px] w-full text-[16px] transition duration-500 ease-in-out transform hover:-translate-y-1 "
-          >
-            {isSubmitting ? <Loader /> : <span>Register</span>}
-          </Button>
-          {formValidMessage && (
-            <div className="event-page-registration-error-message">
-              {formValidMessage}
-            </div>
-          )}
-
-          <p className="mt-[10px] ">
-            Already have an account?{" "}
-            <Link href="/admin-login" className="underline">
-              Login
-            </Link>
-          </p>
-        </form>
-      </div>
+        <p className="mt-[10px]">
+          Already have an account?{" "}
+          <Link href="/admin-login" className="underline">
+            Login
+          </Link>
+        </p>
+      </form>
     </div>
   );
 };
