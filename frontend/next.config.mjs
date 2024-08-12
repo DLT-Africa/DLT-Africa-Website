8d683bc200780b1b805e8174fc4e1e58d9132129
DLT-Africa <142330919+DLT-Africa@users.noreply.github.com>
2024-08-12 13:56:34 +0100

    done

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Set output to 'export' for static HTML export
   output: "export",
+  
+  // Add trailing slashes to URLs
+  trailingSlash: true,
+  
+  // Custom Webpack configuration
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Disable caching
+    // Disable caching for builds
     config.cache = false;
     return config;
   },
