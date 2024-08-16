0dbfe5be85ba05ee36030102d5d5ac160641d579
DLT-Africa <142330919+DLT-Africa@users.noreply.github.com>
2024-08-16 18:30:37 +0100

    ..1.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,27 @@
 "use client";
 
-import dynamic from 'next/dynamic';
-
+import Head from "next/head";
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import CurrentAndUpcoming from "./components/HomePage/CurrentAndUpcoming/CurrentAndUpcoming";
 import Faqs from "./components/HomePage/Faq/Faqs";
 import JoinHackerHouse from "./components/HomePage/JoinHackerHouse/JoinHackerHouse";
-import Partners from "./components/HomePage/Partners/Partners"
-import {
-  RegisterOnline,
-} from "./components/HomePage/Register/Register";
+import Partners from "./components/HomePage/Partners/Partners";
+import { RegisterOnline } from "./components/HomePage/Register/Register";
 import WhatYou from "./components/HomePage/WhatYou/WhatYou";
 import Loader from "./components/Loader/Loader";
-const HeroSection = dynamic(() => import('./components/HomePage/HeroSection/HeroSection'), { ssr: false });
+
+const HeroSection = dynamic(
+  () => import("./components/HomePage/HeroSection/HeroSection"),
+  { ssr: false }
+);
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   useEffect(() => {
     setTimeout(() => {
@@ -25,6 +31,22 @@ export default function Home() {
 
   return (
     <div>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-G2R8DSB4GV"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G2R8DSB4GV');
+            `,
+          }}
+        />
+      </Head>
       {loading ? (
         <Loader />
       ) : (
@@ -32,7 +54,7 @@ export default function Home() {
           <HeroSection />
           <WhatYou />
           <RegisterOnline />
-          <Partners/>
+          <Partners />
           <JoinHackerHouse />
           <Faqs />
           <CurrentAndUpcoming />
