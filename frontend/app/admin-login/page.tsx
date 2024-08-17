5efe66cd39c8da7a89004fa371eceb001bd5021a
ameer017 <rajiabdullahi907@gmail.com>
2024-08-17 23:03:32 +0100

    analytics setup

--- frontend/app/admin-login/page.tsx
+++ frontend/app/admin-login/page.tsx
@@ -1,10 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
 import Login from "../components/Admins/Login"
 const page = () => {
+  useEffect(() => {
 
-  
+    // Google Tag Manager
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-G2R8DSB4GV";
+    script.async = true;
+    document.head.appendChild(script);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-G2R8DSB4GV');
+    `;
+    document.head.appendChild(script2);
+
+    return () => {
+      document.head.removeChild(script);
+      document.head.removeChild(script2);
+    };
+  }, []);
+
+
   return (
     <div>
-      <Login/>
+      <Login />
     </div>
   )
 }
