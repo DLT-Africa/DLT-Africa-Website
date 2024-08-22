c7bef8f69cc40af47a52069ddbbe7df4c7a5f039
ameer017 <rajiabdullahi907@gmail.com>
2024-08-22 14:58:43 +0100

    animations

--- frontend/app/components/Courses/IconSection/IconSection.js
+++ frontend/app/components/Courses/IconSection/IconSection.js
@@ -20,7 +20,7 @@ const IconSection = ({
       setIsSmallDevice(mediaQuery.matches);
     };
     mediaQuery.addListener(handleResize);
-    handleResize(); 
+    handleResize();
     return () => {
       mediaQuery.removeListener(handleResize);
     };
@@ -28,25 +28,38 @@ const IconSection = ({
 
   return (
     <div
-      className={`courseCon ${courseConClass}  cursor-pointer`}
+      className={`courseCon ${courseConClass} cursor-pointer relative`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className={`iconBody ${iconBodyClass} w-[95%] flex justify-center `}>
-        <article className="flex justify-center items-center flex-col transition duration-500 ease-in-out delay-500 transform hover:-translate-y-1 hover:scale-70 ">
-          <div className={`icons ${iconsClass}`}>{icon}</div>
-          <span className={`block ${titleClass}`}>{title}</span>
+      <div className={`iconBody ${iconBodyClass} w-[95%] flex justify-center`}>
+        <article className="flex justify-center items-center flex-col">
+          {/* Icon and Title */}
+          <div
+            className={`icons ${iconsClass} transition-all duration-300 ease-in-out transform delay-500 ${isHovered ? "opacity-0" : "opacity-100"
+              }`}
+          >
+            {icon}
+          </div>
+          <span
+            className={`block ${titleClass} transition-all duration-300 ease-in-out transform delay-500 ${isHovered ? "opacity-0" : "opacity-100"
+              }`}
+          >
+            {title}
+          </span>
+
+          {/* Text  */}
           <h5
-            className={`text-sm ${textClass} ${
-              isSmallDevice || isHovered ? "block" : "hidden"
-            } `}
+            className={`absolute inset-0 flex items-center justify-center text-[12px] ${textClass} transition-all duration-300 ease-in-out transform  delay-500 ${isHovered ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-4"
+              }`}
           >
             {text}
           </h5>
           <h5
-            className={`text-sm ${textClass} ${
-              isSmallDevice || !isHovered ? "block" : "hidden"
-            }`}
+            className={`absolute inset-0 flex items-center justify-center text-[12px] ${textClass} transition-all duration-300 ease-in-out transform delay-500  ${isHovered ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-4"
+              }`}
           >
             {text1}
           </h5>
